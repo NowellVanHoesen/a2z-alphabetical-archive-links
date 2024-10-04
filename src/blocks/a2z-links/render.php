73d43dc1c1e30ad6f9451b68241af9dc6ab1de59
Nowell VanHoesen <nowell@nvwebdev.com>
2024-10-04 12:36:54 -0400

    addressed some phpcs issues and replaced direct html output with printf

--- src/blocks/a2z-links/render.php
+++ src/blocks/a2z-links/render.php
@@ -2,24 +2,22 @@
 
 namespace NVWD\A2ZAAL;
 
-/**
+/*
  * @see https://github.com/WordPress/gutenberg/blob/trunk/docs/reference-guides/block-api/block-metadata.md#render
  */
 
-$container_classes = [];
+$a2zaal_container_classes = [];
 
 if ( $attributes['showCounts'] ) {
-	$container_classes[] = 'counts';
+	$a2zaal_container_classes[] = 'counts';
 }
 
 if ( ! empty( $attributes['title'] ) ) {
-?>
-<h2><?php \esc_html_e( $attributes['title'], 'nvwd-a2zaal' ); ?></h2>
-<?php } ?>
-<ul <?php print \get_block_wrapper_attributes( [ 'class' => \implode( ' ', $container_classes ) ] ); ?>>
-	<?php
-		print \wp_kses_post(
-			implode( '', get_a2zaal_display_links( $attributes['selectedPostType'], $attributes['showCounts'] ) )
-		);
-		?>
-</ul>
+	printf( '<h2>%s</h2>', \esc_html( $attributes['title'] ) );
+}
+
+printf(
+	'<ul %s>%s</ul>',
+	\wp_kses_data( \get_block_wrapper_attributes( [ 'class' => \implode( ' ', $a2zaal_container_classes ) ] ) ),
+	\wp_kses_post( implode( '', get_a2zaal_display_links( $attributes['selectedPostType'], $attributes['showCounts'] ) ) )
+);
